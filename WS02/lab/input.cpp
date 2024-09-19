bbf094bb17a79c7fce97420c9a005b89ee94f1c4
Shaoyu <162636625+MarvelousJade@users.noreply.github.com>
2024-09-18 22:45:15 -0400

    Create frist overloaded function

--- WS02/lab/input.cpp
+++ WS02/lab/input.cpp
@@ -23,6 +23,25 @@ namespace seneca {
       return num;
    }
 
-  
+   int getInt(int minVal, int maxVal) {
+       int num = 0;
+       bool done = false;
+       do {
+           // extract an integer from the console and put it in num
+           cin >> num;
+           // if this action fails (the integer is unreadable)
+           if (cin.fail() || num < minVal || num > maxVal ) {
+               cout << "Bad integer entry, please try again:\n> ";
+               // clear the failure status to activate cin again
+               cin.clear();
+           }
+           else {
+               done = true;
+           }
+           // flush the keyboard buffer up to 1000 characters or '\n', whichever comes first
+           cin.ignore(1000, '\n');
+       } while (!done);
+       return num;
+   }
 }
 
